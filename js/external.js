7f57d5e6bedb7bfcd1a85df4ed591b017c5204c5
Malik <willis.malik.r@gmail.com>
2022-07-28 16:04:05 -0500

    first push

---
+++ js/external.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+
+var favorite_color;
+favorite_color = prompt("What is your favorite color?");
+alert("No way that is my favorite color too! " + "I wear " + favorite_color + " a lot!");
+
+alert("You've order 3 movies. Little Mermaid, Brother Bear, Herculus.");
+
+var price_of_movie = 3;
+var days_rented = 0;
+var movie_amount = 3;
+
+var movie_1 = prompt("How many days do you want to rent Little Mermaid?");
+days_rented = movie_1 + days_rented;
+
+var movie_1 = prompt("How many days do you want to rent Brother Bear?");
+days_rented = movie_1 + days_rented;
+
+var movie_1 = pr
+
+
