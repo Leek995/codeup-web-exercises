f114318164bfeb80eb66225b68ea37659b5816fe
Malik <willis.malik.r@gmail.com>
2022-08-29 12:28:49 -0500

    dom exercieses.

---
+++ js/dom-lecture.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let searchLink = document.getElementById("search-link");
+console.log(searchLink.hasAttribute("href"));
+console.log(searchLink.hasAttribute("class"));
+
+document.get
