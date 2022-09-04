fd393ede407e0b16fca158a6e6378ce4d2aed566
Malik <willis.malik.r@gmail.com>
2022-09-04 17:32:47 -0500

    more practice with dom manipulation

---
+++ js/freecodecamp-dom-practice.js
@@ -0,0 +1,16 @@
+// DOM Manipulation
+// getElementById()
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// getElementsByClass()
+const listItems = document.getElementsByClassName('list-items');
+console.log(listItems);
+
+// querySelector()
+const container = document.querySelector('div');
+console.log(container);
+
+// querySelectorAll()
+const allContainers = document.querySelectorAll('div');
+console.log(allContainers);
