c343a9fa80a98c604e8149a0fb3bc2ed88c6dae2
Malik <willis.malik.r@gmail.com>
2022-09-06 11:07:14 -0500

    exploring jquery mouse events click, dbclick

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,12 +1,22 @@
 "use strict";
 (function(){
-    alert($('#header-one').html());
-    alert($('#content').html());
-    $('.list-items').css('border', '1px solid black');
-    $('#list-items').css('border', '1px solid red');
-    $('li').css('font-size', '20px');
-    $('h1, p, li').css('background', 'yellow');
-    // .html grabs content and possibly changes content too
-    $('h1').html("This is now my header");
-    alert($('#header-one').html());
+    // alert($('#header-one').html());
+    // alert($('#content').html());
+    // $('.list-items').css('border', '1px solid black');
+    // $('#list-items').css('border', '1px solid red');
+    // $('li').css('font-size', '20px');
+    // $('h1, p, li').css('background', 'yellow');
+    // // .html grabs content and possibly changes content too
+    // $('h1').html("This is now my header");
+    // alert($('#header-one').html());
+
+    var fun = function (){
+        console.log("my click event was fired off");
+    }
+    // $('#header-one').click(fun);
+    var content = $('#content');
+    $('#content').dblclick(function (e){
+        alert(content.html());
+    });
+
 })();
