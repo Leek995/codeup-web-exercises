bfed36e73c060d8220689c70f6985a38f0ac326c
Malik <willis.malik.r@gmail.com>
2022-09-06 10:03:45 -0500

    jquery practice with selectors(id, elements, class, and multiple selectors)

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,4 +1,12 @@
 "use strict";
-$(function() {
-    alert('The dom is downloaded!');
-});
+(function(){
+    alert($('#header-one').html());
+    alert($('#content').html());
+    $('.list-items').css('border', '1px solid black');
+    $('#list-items').css('border', '1px solid red');
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css('background', 'yellow');
+    // .html grabs content and possibly changes content too
+    $('h1').html("This is now my header");
+    alert($('#header-one').html());
+})();
