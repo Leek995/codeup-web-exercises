cb188174e0b68ac9f0acf71c54f57f8b0a7fa88d
Malik <willis.malik.r@gmail.com>
2022-09-06 12:01:01 -0500

    completing exercises which puts together all key mouse Events learned in jQuery (hover, click, and doubleclick aka dblclick). Top portion is introduction and snippets. Bottom portion is exercise.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -9,30 +9,51 @@
     // // .html grabs content and possibly changes content too
     // $('h1').html("This is now my header");
     // alert($('#header-one').html());
+    //
+    // var fun = function (){
+    //     console.log("my click event was fired off");
+    // }
+    // // $('#header-one').click(fun);
+    // var content = $('#content');
+    // $('#content').dblclick(function (e){
+    //     alert(content.html());
+    // });
+    //
+    // var subContent = $('#sub-content');
+    // console.log(subContent.html());
+    // var hover_out = function (e){
+    //     subContent.css('background', 'none');
+    // }
+    // var hover_in = function (e){
+    //     subContent.css('background', 'yellow');
+    // }
+    // $('#sub-content').hover( hover_in, hover_out );
+    //
+    // subContent.mouseenter(function (){
+    //     subContent.css('font-size', '50px');
+    // })
+    // subContent.mouseout(function (){
+    //     subContent.css('font-size', '10px');
+    // })
 
-    var fun = function (){
-        console.log("my click event was fired off");
-    }
-    // $('#header-one').click(fun);
-    var content = $('#content');
-    $('#content').dblclick(function (e){
-        alert(content.html());
-    });
-
-    var subContent = $('#sub-content');
-    console.log(subContent.html());
-    var hover_out = function (e){
-        subContent.css('background', 'none');
-    }
-    var hover_in = function (e){
-        subContent.css('background', 'yellow');
-    }
-    $('#sub-content').hover( hover_in, hover_out );
-
-    subContent.mouseenter(function (){
-        subContent.css('font-size', '50px');
+        //                                   Selectors Exercise Start
+    // Change background of h1 when clicked
+    // Gather selector at h1
+    // create event
+    var header_one = $('#header-one');
+    header_one.click(function (){
+        $(this).css('background', 'red');
     })
-    subContent.mouseout(function (){
-        subContent.css('font-size', '10px');
+
+    $('p').dblclick(function (){
+        $(this).css('fontSize', '18px');
     })
+
+    var hoveringIn = function (e){
+        $('li').css('color', 'red');
+    }
+    var hoveringOut = function (e){
+        $('li').css('color', 'black');
+    }
+    $('li').hover(hoveringIn, hoveringOut);
 })();
