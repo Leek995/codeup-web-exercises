0c26a05ee4664ea13cd092ae86c5594cd62801a0
Malik <willis.malik.r@gmail.com>
2022-09-09 00:00:15 -0500

    remainder studies

--- js/eloquentJs.js
+++ js/eloquentJs.js
@@ -40,11 +40,32 @@
 // //before function returned undefined it waited on the next binding to complete in console.log
 // console.log(twice(5));
 
-function power(base, exponent){
-    if(exponent == 0){
-        return 1;
-    }else{
-        return base * power(base, exponent - 1);
+// function power(base, exponent){
+//     if(exponent == 0){
+//         return 1;
+//     }else{
+//         return base * power(base, exponent - 1);
+//     }
+// }
+// console.log(power(2,3));
+
+function findSolution(target){
+    function find(current, history){
+        if(current == target){
+            return history;
+        }else if(current > target){
+            return null;
+        }else{
+            return find(current + 5, `(${history} + 5)`) ||
+            find(current * 3, `(${history} * 3)`);
+        }
     }
+    return find(1, "1");
 }
-console.log(power(2,3));
+
+console.log(findSolution(24));
+
+function printFarmInventory(cows, chicken){
+    let cowString = String(cows);
+    while()
+}
