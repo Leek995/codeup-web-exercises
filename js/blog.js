67681a0b92be415e4550622a5b8adff8263fc7e4
Malik <willis.malik.r@gmail.com>
2022-09-11 03:18:53 -0500

    categories in list format

--- js/blog.js
+++ js/blog.js
@@ -4,29 +4,47 @@
             // requesting file data from server
         function renderHTML(blogPost) {
             let content = '';
+
             for(let i = 0; i < blogPost.length; ++i){
-                content += `<div id="post">\n
-                              <h1>${blogPost[i]['title']}</h1>\n
-                              <textarea>${blogPost[i]['content']}</textarea>\n
-                              <ul>\n
-                                  <li>${blogPost[i]['categories']}</li>\n
-                              </ul>\n
-                              <input type="date" id="start" value="2022-09-10" min="2020-09-01" max="2025-09-01">\n
-                          </div>`;
+                if(blogPost[i]['categories'].length === 1){
+                    content += `<div id="post">\n
+                            \t<h1>${blogPost[i]['title']}</h1>\n
+                            \t<textarea>${blogPost[i]['content']}</textarea>\n
+                            \t<p>${blogPost[i]['categories']}</p>\n
+                            \t<input type="date" id="start" value="2022-09-10" min="2020-09-01" max="2025-09-01">\n
+                            </div>`;
+                }else if(blogPost[i]['categories'].length > 1)
+                {
+                    content += `<div id="post">\n
+                            \t<h1>${blogPost[i]['title']}</h1>\n
+                            \t<textarea>${blogPost[i]['content']}</textarea>\n
+                            \t<p>${blogPost[i]['categories'][0]}</p>\n
+                            \t<p>${blogPost[i]['categories'][1]}</p>\n
+                            \t<p>${blogPost[i]['categories'][2]}</p>\n
+                            \t<p>${blogPost[i]['categories'][3]}</p>\n
+                            \t<p>${blogPost[i]['categories'][4]}</p>\n
+                            \t<input type="date" id="start" value="2022-09-10" min="2020-09-01" max="2025-09-01">\n
+                            </div>`;
+
+
+
+                }
+
+
+
+
+
             }
             return content;
         }
-        function postCat(blogPost){
-            blogPost.forEach(function (post){
-                if(post['categories'] < 1){
-                    console.log(`categories are greater than one`);
-                }
-            })
-        }
+
         function getDataLoad() {
             $.get('../data/blog.json')
                 .done(function (blogPost) {
-                    console.log(blogPost);
+                    for(let i = 0; i < blogPost[0]['categories'].length; ++i){
+                        console.log(blogPost[0]['categories'][i]);
+                    }
+
                     let blogPostHTML = renderHTML(blogPost);
                     $('#post').html(blogPostHTML);
                     console.log(blogPost[0]['categories']);
