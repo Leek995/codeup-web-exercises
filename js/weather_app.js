6439a09721193cc1e60cdd202c2ffcf8888b4ab6
Malik <willis.malik.r@gmail.com>
2022-09-13 14:20:53 -0500

    ordered 3hr forecast into 5-day forcast format

--- js/weather_app.js
+++ js/weather_app.js
@@ -5,4 +5,52 @@
 (function (){
     "use strict"
     console.log('testing conection');
+    // 1.) Update your layout and AJAX request(s) to display a five day forecast for the city you
+    //      live in that looks like the screenshot below.
+    //TODO
+    // create a function that takes in weather data as a argument
+    // inetrate through list to find list of 3hr forecast. single out list and store in new binding
+    // convert 3hr forecast to 1Day forecast
+    // iterate through
+
+    // Separates weather data from all other datasets in JSON object from OpenWeather API
+    function fiveDayForeCast(weatherData){
+        // stores weather 3hr weather forecast in binding
+        let forecast = weatherData['list'];
+        // console.log(forecast);
+        // let day_forecast = [];
+        // day_forecast = forecast.splice(0,8);
+        // console.log(day_forecast);
+        // create empty list to store 5-day forecast
+        let five_day_forecast = [];
+        // iterate through 3hr weather array to convert to a day format of 3hr forecast for next 5-days
+        while(forecast.length){
+            // create empty array to act as temporary storage to transfer to 5-day forecast array
+            let day_forecast = [];
+            // conditional to control flow of while loop for one DAY
+            if(forecast.length % 8 === 0) {
+                // REMOVE first 8 indexes/elements from forecast array & ADD them 5-day forecast array
+                day_forecast = forecast.splice(0, 8);
+                five_day_forecast.push(day_forecast);
+                //counter for while loop to eventually terminate
+                forecast.length - 8;
+            }else {
+                break;
+            }
+        }
+        console.log(five_day_forecast);
+    }
+
+    $.get('https://api.openweathermap.org/data/2.5/forecast',{
+        appid: WEATHER_APP_ID,
+        lon: -87.083321,
+        lat: 32.333469,
+        unit: 'imperial',
+    }).done(function (data){
+        console.log(data);
+        fiveDayForeCast(data);
+    }).fail(function (error){
+        console.log(error);
+    })
+
 })();
