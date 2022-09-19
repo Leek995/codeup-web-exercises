f4293dc3d69dd13ef68be8b2f2237806be17f548
Malik <willis.malik.r@gmail.com>
2022-09-19 12:10:52 -0500

    completed filter portion of exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,47 @@
+(function (){
+    "use strict"
+    const users = [
+        {
+            id: 1,
+            name: 'ryan',
+            email: 'ryan@codeup.com',
+            languages: ['clojure', 'javascript'],
+            yearsOfExperience: 5
+        },
+        {
+            id: 2,
+            name: 'luis',
+            email: 'luis@codeup.com',
+            languages: ['java', 'scala', 'php'],
+            yearsOfExperience: 6
+        },
+        {
+            id: 3,
+            name: 'zach',
+            email: 'zach@codeup.com',
+            languages: ['javascript', 'bash'],
+            yearsOfExperience: 7
+        },
+        {
+            id: 4,
+            name: 'fernando',
+            email: 'fernando@codeup.com',
+            languages: ['java', 'php', 'sql'],
+            yearsOfExperience: 8
+        },
+        {
+            id: 5,
+            name: 'justin',
+            email: 'justin@codeup.com',
+            languages: ['html', 'css', 'javascript', 'php'],
+            yearsOfExperience: 9
+        }
+    ];
+    // Use .filter to create an array of user objects where each user
+    // object has at least 3 languages in the languages array.
+    let arr = [];
+    let langArr = users.filter(function (n) {
+        if(n.languages.length > 2) console.log(n.languages);
+    })
+
+})();
---
+++ js/sum_range.js
@@ -0,0 +1,9 @@
+(function (){
+    "use strict"
+    function sumOfRange(start, end){
+        let sum;
+        let range;
+
+    }
+    sumOfRange(1, 10);
+})();
